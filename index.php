99b58438727e88d721819b8a83136a1804976501
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-26 11:38:02 -0600

    In-class exercise/example

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"
+		<--Adding CSS-->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<title>CSS Demo!</title>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<a href="#">CSS DEMO</a>
+				<ul>
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+				</ul>
+			</div>
+		</header>
+
+		<main>
+			<div class="container">
+				<p class="width-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum aliquet ante porta accumsan. Nullam rhoncus scelerisque magna vel sodales. Phasellus vulputate risus sollicitudin turpis fringilla, sed pellentesque massa malesuada. Nulla in ex vel nisl lacinia gravida eget non lorem. Mauris congue tempus tellus, a tempus lectus commodo eu. Sed elementum ligula in dapibus lacinia. Sed finibus erat ut risus condimentum imperdiet. Duis et elementum augue. Vestibulum ultrices leo non nulla cursus, nec posuere velit bibendum. Nulla blandit elit quis lacus dapibus, vitae vehicula eros facilisis. Aenean molestie eros non lorem tempor pretium. Aenean congue tellus diam, ac commodo eros pretium nec. </p>
+				<p class="width-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum aliquet ante porta accumsan. Nullam rhoncus scelerisque magna vel sodales. Phasellus vulputate risus sollicitudin turpis fringilla, sed pellentesque massa malesuada. Nulla in ex vel nisl lacinia gravida eget non lorem. Mauris congue tempus tellus, a tempus lectus commodo eu. Sed elementum ligula in dapibus lacinia. Sed finibus erat ut risus condimentum imperdiet. Duis et elementum augue. Vestibulum ultrices leo non nulla cursus, nec posuere velit bibendum. Nulla blandit elit quis lacus dapibus, vitae vehicula eros facilisis. Aenean molestie eros non lorem tempor pretium. Aenean congue tellus diam, ac commodo eros pretium nec. </p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum aliquet ante porta accumsan. Nullam rhoncus scelerisque magna vel sodales. Phasellus vulputate risus sollicitudin turpis fringilla, sed pellentesque massa malesuada. Nulla in ex vel nisl lacinia gravida eget non lorem. Mauris congue tempus tellus, a tempus lectus commodo eu. Sed elementum ligula in dapibus lacinia. Sed finibus erat ut risus condimentum imperdiet. Duis et elementum augue. Vestibulum ultrices leo non nulla cursus, nec posuere velit bibendum. Nulla blandit elit quis lacus dapibus, vitae vehicula eros facilisis. Aenean molestie eros non lorem tempor pretium. Aenean congue tellus diam, ac commodo eros pretium nec. </p>
+			</div>
+		</main>
+	</body>
+</html>
